0ce28be6837cde99ca71dd6130a91282e488b944
micheledinelli <dinellimichele00@gmail.com>
2023-10-06 19:22:01 +0200

    Added download for drinks and wines menus

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
+import DrinksMenu from "../assets/menu-drinks.pdf";
+import DownloadMenu from "../components/DownloadMenu";
+
 export default function Drinks() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <DownloadMenu file={DrinksMenu} filename={"meobar-drinks"} />
+    </>
   );
 }
--- src/pages/Wines.jsx
+++ src/pages/Wines.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import DownloadMenu from "../components/DownloadMenu";
+
+import WinesMenu from "../assets/menu-wines.pdf";
 
 export default function Wines() {
   return (
     <div>
       <Navbar />
+      <DownloadMenu file={WinesMenu} filename={"meobar-drinks"}></DownloadMenu>
     </div>
   );
 }
