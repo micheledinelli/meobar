8197ac63aeeb3a895a0e291be939f9bd35394467
benzebra <filippobrajucha@gmail.com>
2023-10-09 17:08:30 +0200

    map push

--- src/pages/ReachUs.jsx
+++ src/pages/ReachUs.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Map from "../components/Map"
+import TimeTable from "../components/TimeTable"
 
 export default function ReachUs() {
-  return <>ca</>;
+  return (
+    <div>
+      <Navbar />
+      <Map />
+      <TimeTable />
+    </div>
+  );
 }
