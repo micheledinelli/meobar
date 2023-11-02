658c63c29e3b855f6ac76fe8d802d9fb42f3c12e
micheledinelli <dinellimichele00@gmail.com>
2023-11-02 21:10:43 +0100

    added drinklist.json

--- src/components/HeroDrinks.jsx
+++ src/components/HeroDrinks.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import MenuDrinks from "../assets/menu-drinks.pdf";
 import DownloadButton from "../components/buttons/DownloadButton";
 
+import drinklist from "../constants/drinklist.json";
+
 export default function HeroDrinks() {
   return (
-    <div>
-      <div className="flex flex-col justify-center content-center my-5 w-1/2">
-        <p className="mt-10 fade-in text-5xl">Our drinks</p>
-      </div>
+    <div className="w-full bg-red-500">
+      <p className="mt-10 fade-in text-5xl">Our drinks</p>
+
+      {/* <div className="flex flex-row">{console.log(drinklist.map(elem) => (cons))}</div> */}
     </div>
   );
 }
